56bdc2fc11a3a19e329997fe66bc9703fa64f6f9
Nick Packham <46549847+NickPackham@users.noreply.github.com>
2020-05-04 17:48:27 -0700

    Update config.lua

--- lua/voy_citadel_config/config.lua
+++ lua/voy_citadel_config/config.lua
@@ -2,7 +2,7 @@ ANG_GM_Citadel = {}
 
 -- The Citadel command is !citadel # (Loadout Number seen below with the NPC's.)
 
-ANG_GM_Citadel_Config_Version = 9
+ANG_GM_Citadel_Config_Version = 12
 
 ANG_GM_Citadel_Admins = {
 	["superadmin"] = true,
